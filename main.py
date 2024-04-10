dd296751699ff5765ee8747c56ccbbf88c5f8e35
Antony Praveenkumar M <antonykumar15898@gmail.com>
2024-04-10 12:57:08 +0530

    open the google.com after 10 sec and quite

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+import time
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
+
+# Open a site using `driver`
+
+driver.get("https://google.com")
+
+time.sleep(10)
+
+driver.quite()
