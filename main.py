1cf94144a0da9b62ef782ca8c7b867ee8ccf35b2
Antony Praveenkumar M <antonykumar15898@gmail.com>
2024-04-11 12:30:51 +0530

    CookiClicker its a small project to automate the process

--- main.py
+++ main.py
@@ -23,6 +23,15 @@ input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
 input_element.clear() # Clear the field fiels instead of override
 input_element.send_keys("tech with tim" + Keys.ENTER)
 
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Tech With Tim"))
+) # After 5 Sec the element we can't able find the element means crash the program or go head for next move
+
+
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Tech With Tim") # Find the Link With Text & the `PARTIAL_LINK_TEXT` finds the if a text contains inside the `anchor` link if you want to check the exact text you can use the `LINK_TEXT`
+link.click() # Click the Link
+
+
 
 time.sleep(10)
 
